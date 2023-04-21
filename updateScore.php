f7a62c2aff90ba0fbea3f9adb867b57063eabe39
TRAN NGUYEN <109562641+nhokboylun@users.noreply.github.com>
2023-04-20 21:53:49 -0400

    Add files via upload

--- updateScore.php
+++ updateScore.php
@@ -2,7 +2,7 @@
 // Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
-    $score = $_POST["score"];
+    $score = (int)$_POST["score"];
     
     $filename = "score.txt";
     
@@ -12,20 +12,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Split the content into an array of lines
     $lines = explode("\n", $file_content);
 
-    // Find the existing score for the player, if available
-    $player_found = false;
-    for ($i = 0; $i < count($lines); $i++) {
-        if (strpos($lines[$i], $name) !== false) {
-            $lines[$i] = $name . ": " . $score.",";
-            $player_found = true;
-            break;
+    // Create an associative array with player names as keys and their scores as values
+    $scores = [];
+    foreach ($lines as $line) {
+        if (!empty($line)) {
+            list($player_name, $player_score) = explode(": ", $line);
+            $player_score = (int)trim($player_score, ",");
+            $scores[$player_name] = $player_score;
         }
     }
-    if ($player_found === true){
-    // Join the updated lines and write them back to the file
-    $updated_content = implode("\n", $lines);
-    file_put_contents($filename, $updated_content);
+
+    // Update the player's score if the new score is higher
+    if (array_key_exists($name, $scores)) {
+        if ($score > $scores[$name]) {
+            $scores[$name] = $score;
+        }
+    } else {
+        $scores[$name] = $score;
     }
+
+    // Sort the scores in descending order
+    arsort($scores);
+
+    // Write the sorted scores back to the file
+    $updated_content = '';
+    foreach ($scores as $player_name => $player_score) {
+        $updated_content .= $player_name . ": " . $player_score . ",\n";
+    }
+    file_put_contents($filename, $updated_content);
 } else {
     http_response_code(405); // Method Not Allowed
 }
